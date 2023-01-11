549beb9541b19a4553c238ef29451b1f7de0244a
Febrilian <febrilian.kr@gmail.com>
2023-01-11 18:10:27 +0700

    Custom config only

---
+++ plugins/lspconfig.lua
@@ -0,0 +1,14 @@
+-- custom.plugins.lspconfig
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local lspconfig = require "lspconfig"
+
+-- Server Configurations https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local servers = {"html", "cssls", "clangd", "gopls", "tsserver", "emmet_ls", "sumneko_lua"}
+
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities
+    }
+end
